6293faff894975df3ad5cd2e079da8f03130f910
AnderG <agibarguen@gmail.com>
2022-07-10 20:04:56 +0200

    Count and user prompt debug repaired. Five rounds evaluation implemented. Only images left.

--- index.js
+++ index.js
@@ -9,86 +9,61 @@ const userEl = document.getElementById("user-txt")
 userEl.textContent += credits
 const computerEl = document.getElementById("computer-txt")
 const titleEl = document.getElementById("title-el")
-
-const images = ["rock.png","paper.png","scissors.png"]
-const userImg = document.getElementById("img1")
-const computerImg = document.getElementById("img2")
-
-//render images inside div
-
-function renderUserImg(){
-    if(playerItem==="ROCK"){ userImg.textContent = `<img class="game-img" src="${images[0]}" alt="rock image">`}
-    else if(playerItem==="PAPER"){userImg.textContent = `<img class="game-img" src="${images[1]}" alt="paper image">`}
-    else if(playerItem==="SCISSORS"){userImg.textContent = `<img class="game-img" src="${images[2]}" alt="scissors image">`}
-}
-function renderComputerImg(){
-    if(randomItem==="ROCK"){computerImg.textContent = `<img class="game-img" src="${images[0]}" alt="rock image">`}
-    else if(randomItem==="PAPER"){computerImg.textContent = `<img class="game-img" src="${images[1]}" alt="paper image">`}
-    else if(randomItem==="SCISSORS"){computerImg.textContent = `<img class="game-img" src="${images[2]}" alt="scissors image">`}
-}
+//function that lets the user play only 5 rounds
+const fiveRounds = ["First Round","Second Round", "Third Round", "Fourth Round", "Last Round"]
+let click = -1
 
 //once the start-btn in clicked render randomly after the computer: using .textcontent an intem from the rockPaperScissors array
 startEl.addEventListener("click", function computerItem(){
-    let playerItem = prompt("Rock, Paper or Scissors?", ).toUpperCase()
-    randomItem = rockPaperScissors[Math.floor(Math.random()*rockPaperScissors.length)].toUpperCase()
-    computerEl.textContent = `The Computer: ${randomItem}`
-    computerPlay(playerItem, randomItem)
-    userCredits()
-    renderUserImg()
-    renderComputerImg()
-
+    click += 1
+    if(click <fiveRounds.length){
+        startEl.textContent = fiveRounds[click]
+    } else if (click >= fiveRounds.length){   
+        isPlaying = false
+        startEl.textContent = `Game Over`
+    }
+    if (isPlaying===true){
+        let playerItem = prompt("Rock, Paper or Scissors?", ).toUpperCase()
+        randomItem = rockPaperScissors[Math.floor(Math.random()*rockPaperScissors.length)].toUpperCase()
+        computerEl.textContent = `The Computer: ${randomItem}`
+        computerPlay(playerItem, randomItem)
+    }
+    else if(isPlaying===false){
+        titleEl.textContent = `Game Over B****`
+        userEl.textContent = `You: `
+        computerEl.textContent = `The Computer: `
+    }
 })
 
 //compare the user election and the computers one. MANERA MAS FACIL? SIN HARDCODEAR TANTO?
-function computerPlay (playerSelection, computerSelection){
-    if(playerSelection==="ROCK" && randomItem==="ROCK"){ 
-        titleEl.textContent = `No one wins. Both are equal`
-    } 
-    else if(playerSelection==="ROCK" && randomItem==="PAPER"){
-        titleEl.textContent = `You lose! ${randomItem} beats ${playerSelection}. You lose 25$.`
-    }
-    else if(playerSelection==="ROCK" && randomItem==="SCISSORS"){
-        titleEl.textContent = `You win! ${playerSelection} beats ${randomItem}. You earn 10$.`
-    }
-    else if(playerSelection==="PAPER" && randomItem==="ROCK"){
-        titleEl.textContent = `You win! ${playerSelection} beats ${randomItem}. You earn 10$.`
-    }
-    else if(playerSelection==="PAPER" && randomItem==="PAPER"){
-        titleEl.textContent = `No one wins. Both are equal`
-    }
-    else if(playerSelection==="PAPER" && randomItem==="SCISSORS"){
-        titleEl.textContent = `You lose! ${randomItem} beats ${playerSelection}. You lose 25$.`
-    }
-    else if(playerSelection==="SCISSORS" && randomItem==="ROCK"){
-        titleEl.textContent = `You lose! ${randomItem} beats ${playerSelection}. You lose 25$.`
-    }
-    else if(playerSelection==="SCISSORS" && randomItem==="PAPER"){
-        titleEl.textContent = `You win! ${playerSelection} beats ${randomItem}. You earn 10$.`
-    }
-    else if(playerSelection==="SCISSORS" && randomItem==="SCISSORS"){
-        titleEl.textContent = `No one wins. Both are equal`
-    }
-}
-
 //function that evaluates the credits of the user
-
-function userCredits(){
-    playerItem = prompt("Rock, Paper or Scissors?", ).toUpperCase()
-    if(titleEl.textContent === `No one wins. Both are equal`){
-        credits = 200
-
-    } else if(titleEl.textContent === `You lose!. ${randomItem} beats ${playerItem}. You lose 25$.`){
+function computerPlay (playerSelection, computerSelection){ // if both are equal it returs to default value 200$
+    if((playerSelection==="ROCK" && randomItem==="ROCK")||(playerSelection==="PAPER" && randomItem==="PAPER")||(playerSelection==="SCISSORS" && randomItem==="SCISSORS")){ 
+        titleEl.textContent = `No one wins. Both are equal`
+        
+    } 
+    else if((playerSelection==="ROCK" && randomItem==="PAPER")||(playerSelection==="PAPER" && randomItem==="SCISSORS")||(playerSelection==="SCISSORS" && randomItem==="ROCK")){
+        titleEl.textContent = `You lose! ${randomItem} beats ${playerSelection}. You lose 25$.`
         credits = credits - 25
-
-    } else if(titleEl.textContent === `You win!.${playerItem} beats ${randomItem}. You earn 10$.`){
+    }
+    else if((playerSelection==="ROCK" && randomItem==="SCISSORS")||(playerSelection==="PAPER" && randomItem==="ROCK")||(playerSelection==="SCISSORS" && randomItem==="PAPER")){
+        titleEl.textContent = `You win! ${playerSelection} beats ${randomItem}. You earn 10$.`
         credits = credits + 10
     }
     userEl.textContent = `Your credits: $ ${credits}`
 }
 
+
 //once the stop-btn is clicked isPlaying = false and everything will return to default values
 restartEl.addEventListener("click", function restartGame(){
+    if (isPlaying===false){
+    isPlaying = true
     titleEl.textContent = `Rock, Paper, Scissors`
     userEl.textContent = `You: `
-    computerEl.textContent = `The Computer: `
+    computerEl.textContent = `The Computer: `    
+    click = -1
+    startEl.textContent = `Start Game`
+    }
+    
 })
+
